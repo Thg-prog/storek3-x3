b6706d03a90517c17d392c8d72e596088851810b
Thg-prog <84495646+Thg-prog@users.noreply.github.com>
2023-09-30 01:29:18 +0300

    Add GTest to main.cpp file

--- programming sem3/lab2Tree/main.cpp
+++ programming sem3/lab2Tree/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <gtest/gtest.h>
 #include "ThreadBST.h"
 
-int main() {
+
+// Тест конструктора по умолчанию
+TEST(ThreadSafeBSTTest, DefaultConstructor) {
+ThreadSafeBST<int> tree;
+// Проверяем, что дерево пустое
+ASSERT_TRUE(tree.find(42) == false);
+}
+
+// Тест вставки элемента и поиска
+TEST(ThreadSafeBSTTest, InsertAndFind) {
+ThreadSafeBST<int> tree;
+tree.insert(42);
+// Проверяем, что элемент 42 присутствует в дереве
+ASSERT_TRUE(tree.find(42) == true);
+// Проверяем, что другой элемент 24 отсутствует в дереве
+ASSERT_TRUE(tree.find(24) == false);
+}
+
+// Тест удаления элемента
+TEST(ThreadSafeBSTTest, Remove) {
+ThreadSafeBST<int> tree;
+tree.insert(42);
+tree.remove(42);
+// Проверяем, что элемент 42 удален из дерева
+ASSERT_TRUE(tree.find(42) == false);
+}
+
+// Тест оператора <<
+TEST(ThreadSafeBSTTest, OperatorStream) {
+ThreadSafeBST<int> tree;
+tree.insert(42);
+tree.insert(24);
+std::stringstream ss;
+ss << tree;
+// Проверяем, что оператор << выводит элементы дерева в правильном порядке
+ASSERT_EQ(ss.str(), "24 42 ");
+}
+
+int main(int argc, char**argv) {
     ThreadSafeBST<int> tree;
     tree.insert(5);
     tree.insert(3);
@@ -18,6 +57,7 @@ int main() {
     for (const int& value : postorderResult) {
         std::cout << value << " ";
     }
+    testing::InitGoogleTest(&argc,argv);
 // Вывод: 2 4 3 6 8 7 5
-
+    return RUN_ALL_TESTS();
 }
