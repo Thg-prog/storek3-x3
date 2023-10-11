5f10d7576e75d820a7ce729f45c17994af1c7d9c
Thg-prog <84495646+Thg-prog@users.noreply.github.com>
2023-10-11 22:03:54 +0300

    like boss push good not full version

--- programming sem3/GoDiffLab/main.go
+++ programming sem3/GoDiffLab/main.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
+
+type Difference struct {
+	Type  string
+	Start int
+	End   int
+	Text  string
+}
+
+type DifferenceBlock struct {
+	StartLineNumber int
+	EndLineNumber   int
+}
 
 func main() {
 	file1, err1 := ioutil.ReadFile("1.txt")
@@ -22,21 +35,26 @@ func main() {
 	lines1 := strings.Split(string(file1), "\n")
 	lines2 := strings.Split(string(file2), "\n")
 
-	result := make([]string, 0)
+	differences := make([]Difference, 0)
+	blocks := make([]DifferenceBlock, 0)
+	currentBlock := DifferenceBlock{}
 
 	i, j := 0, 0
+	lineNumber1, lineNumber2 := 1, 1
 
 	for i < len(lines1) && j < len(lines2) {
 		if lines1[i] == lines2[j] {
-			result = append(result, "  "+lines1[i])
 			i++
 			j++
+			lineNumber1++
+			lineNumber2++
 		} else {
 			foundDiff := false
 			for x := i + 1; x < len(lines1); x++ {
 				if lines1[x] == lines2[j] {
 					for y := i; y < x; y++ {
-						result = append(result, "< "+lines1[y])
+						differences = append(differences, Difference{Type: "<", Start: lineNumber1, End: lineNumber1, Text: lines1[y]})
+						lineNumber1++
 					}
 					i = x
 					foundDiff = true
@@ -48,7 +66,8 @@ func main() {
 				for x := j + 1; x < len(lines2); x++ {
 					if lines2[x] == lines1[i] {
 						for y := j; y < x; y++ {
-							result = append(result, "> "+lines2[y])
+							differences = append(differences, Difference{Type: ">", Start: lineNumber2, End: lineNumber2, Text: lines2[y]})
+							lineNumber2++
 						}
 						j = x
 						foundDiff = true
@@ -58,26 +77,76 @@ func main() {
 			}
 
 			if !foundDiff {
-				result = append(result, "< "+lines1[i])
-				result = append(result, "> "+lines2[j])
+				if currentBlock.StartLineNumber == 0 {
+					currentBlock.StartLineNumber = lineNumber1
+				}
+				currentBlock.EndLineNumber = lineNumber1
+				differences = append(differences, Difference{Type: "<", Start: lineNumber1, End: lineNumber1, Text: lines1[i]})
+				differences = append(differences, Difference{Type: ">", Start: lineNumber2, End: lineNumber2, Text: lines2[j]})
 				i++
 				j++
+				lineNumber1++
+				lineNumber2++
 			}
 		}
 	}
 
+	// Проверьте, есть ли текущий блок различий, и добавьте его в массив
+	if currentBlock.StartLineNumber > 0 {
+		blocks = append(blocks, currentBlock)
+	}
+
 	for i < len(lines1) {
-		result = append(result, "< "+lines1[i])
+		differences = append(differences, Difference{Type: "<", Start: lineNumber1, End: lineNumber1, Text: lines1[i]})
 		i++
+		lineNumber1++
 	}
 
 	for j < len(lines2) {
-		result = append(result, "> "+lines2[j])
+		differences = append(differences, Difference{Type: ">", Start: lineNumber2, End: lineNumber2, Text: lines2[j]})
 		j++
+		lineNumber2++
 	}
 
-	fmt.Println("result")
-	for _, line := range result {
-		fmt.Println(line)
+	// Сортировка массива различий по полю Start
+	sort.Slice(differences, func(i, j int) bool {
+		return differences[i].Start < differences[j].Start
+	})
+
+	fmt.Println("Результат сравнения:")
+	for i := 0; i < len(differences); {
+		// Проверим, следует ли создать блок для строк с type ">"
+		if differences[i].Type == ">" {
+			startIndex := i
+			endIndex := i
+			for endIndex < len(differences) && differences[endIndex].Type == ">" {
+				endIndex++
+			}
+			// Добавим информацию о блоке
+			blocks = append(blocks, DifferenceBlock{StartLineNumber: differences[startIndex].Start, EndLineNumber: differences[endIndex-1].End})
+			// Выведем блок
+			fmt.Printf("%d a %d,%d\n", differences[startIndex].Start, differences[startIndex].Start, differences[endIndex-1].End)
+			for j := startIndex; j < endIndex; j++ {
+				fmt.Printf("%d %s %s\n", differences[j].Start, differences[j].Type, differences[j].Text)
+			}
+			i = endIndex
+		} else if differences[i].Type == "<" {
+			startIndex := i
+			endIndex := i
+			for endIndex < len(differences) && differences[endIndex].Type == "<" {
+				endIndex++
+			}
+			// Добавим информацию о блоке
+			blocks = append(blocks, DifferenceBlock{StartLineNumber: differences[startIndex].Start, EndLineNumber: differences[endIndex-1].End})
+			// Выведем блок
+			fmt.Printf("%d d %d,%d\n", differences[startIndex].Start, differences[startIndex].Start, differences[endIndex-1].End)
+			for j := startIndex; j < endIndex; j++ {
+				fmt.Printf("%d %s %s\n", differences[j].Start, differences[j].Type, differences[j].Text)
+			}
+			i = endIndex
+		} else {
+			fmt.Printf("%d %s %s\n", differences[i].Start, differences[i].Type, differences[i].Text)
+			i++
+		}
 	}
 }
