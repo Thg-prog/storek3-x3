b6ac621dbc6722eded8103d3cc9f460d290468f6
Ostapenko Igor <skeleton1212@yandex.ru>
2023-10-26 12:21:38 +0300

    done gradient

--- vycmat/4lab/main.py
+++ vycmat/4lab/main.py
@@ -12,14 +12,14 @@ def dz_dx(x, y):
 def dz_dy(x, y):
     return 12*y**2
 
-def gradient_descent(x_start, y_start, learning_rate):
+def gradient_descent(x_start, y_start, lmd):
     x_list = [x_start]
     y_list = [y_start]
     z_list = [z_func(x_start, y_start)]
 
     while True:
-        new_x = x_list[-1] - learning_rate*dz_dx(x_list[-1], y_list[-1])
-        new_y = y_list[-1] - learning_rate*dz_dy(x_list[-1], y_list[-1])
+        new_x = x_list[-1] - lmd*dz_dx(x_list[-1], y_list[-1])
+        new_y = y_list[-1] - lmd*dz_dy(x_list[-1], y_list[-1])
 
         x_list.append(new_x)
         y_list.append(new_y)
@@ -30,11 +30,11 @@ def gradient_descent(x_start, y_start, learning_rate):
         
     return x_list, y_list, z_list
 
-x_start = 2
-y_start = 2
-learning_rate = 0.01
+x_start = 2.5
+y_start = 2.5
+lmd = 0.005
 
-x_list, y_list, z_list = gradient_descent(x_start, y_start, learning_rate)
+x_list, y_list, z_list = gradient_descent(x_start, y_start, lmd)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -50,10 +50,11 @@ ani = FuncAnimation(fig, update, frames=len(x_list), interval=10)
 
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
-ax.set_zlim(0, 50)
+ax.set_zlim(0, 40)
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('z')
 ax.plot_surface(np.arange(-5, 5, 0.1), np.arange(-5, 5, 0.1)[:,np.newaxis], z_func(np.arange(-5, 5, 0.1)[:,np.newaxis], np.arange(-5, 5, 0.1)[np.newaxis,:]), alpha=0.5)
+ax.view_init(elev=10, azim=88)
 plt.title('Gradient Descent')
 plt.show()
