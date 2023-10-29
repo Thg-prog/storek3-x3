d13f734f32c88a3569ef87a9c6cbe62f9995b870
Thg-prog <84495646+Thg-prog@users.noreply.github.com>
2023-10-29 16:25:23 +0300

    Add newton method

--- comput_mat/HalfDevision/src/main.rs
+++ comput_mat/HalfDevision/src/main.rs
@@ -1,16 +1,23 @@
 fn main() {
-    let f = |x: f64| 2.0 * x / (x * x + 1.0);
+    let f = |x: f64| x.powi(3) + 4.0 * x - 3.0;
 
     let mut a = -10.0; // Левая граница интервала
     let mut b = 10.0; // Правая граница интервала
     let epsilon = 1e-6; // Заданная точность
 
+    let root_bisection = find_root_bisection(&f, a, b, epsilon);
+    println!("Приближенный корень с использованием половинного деления: {}", root_bisection);
+
+    let root_newton = find_root_newton(&f, 0.0, epsilon);
+    println!("Приближенный корень с использованием метода Ньютона: {}", root_newton);
+}
+
+fn find_root_bisection(f: &dyn Fn(f64) -> f64, mut a: f64, mut b: f64, epsilon: f64) -> f64 {
     while b - a > epsilon {
         let midpoint = (a + b) / 2.0;
         if f(midpoint) == 0.0 {
             // Найден корень
-            println!("Корень: {}", midpoint);
-            break;
+            return midpoint;
         } else if f(a) * f(midpoint) < 0.0 {
             b = midpoint;
         } else {
@@ -18,6 +25,14 @@ fn main() {
         }
     }
 
-    let root = (a + b) / 2.0;
-    println!("Приближенный корень: {}", root);
+    (a + b) / 2.0
+}
+
+fn find_root_newton(f: &dyn Fn(f64) -> f64, mut x0: f64, epsilon: f64) -> f64 {
+    let mut x = x0;
+    while f(x).abs() > epsilon {
+        let derivative = (f(x + epsilon) - f(x)) / epsilon;
+        x = x - f(x) / derivative;
+    }
+    x
 }
