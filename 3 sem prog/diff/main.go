4bc2f327979caf5573de606e630b57c8e027b7e2
Ostapenko Igor <skeleton1212@yandex.ru>
2023-10-29 13:07:41 +0300

    a, c, d

--- 3 sem prog/diff/main.go
+++ 3 sem prog/diff/main.go
@@ -68,55 +68,32 @@ func getTable(ar1 []string, ar2 []string) [][]string{
         }
     }
     if len(ar2) != 0 {
-        for j := len(ar2)-1; j >= 0; j-- {
-            if arString[len(ar1)-1][j] == " " {
-                arString[len(ar1)-1][j] = "a"
-            } else {
-                break
+        flag := false 
+        for i := 0; i < len(ar2); i++ {
+            if arString[len(ar1)-1][i] == "ok" {
+                flag = true
+            }
+        }
+        if flag {
+            for j := len(ar2)-1; j >= 0; j-- {
+                if arString[len(ar1)-1][j] == " " {
+                    flag = true
+                    for i := 0; i < len(ar1); i++ {
+                        if arString[i][j] != " " {
+                            flag = false
+                        }
+                    }
+                    if flag {
+                        arString[len(ar1)-1][j] = "a"
+                    }
+                } else {
+                    break
+                }
             }
         }
     }
     return arString
 }
-
-// // получение удалённых строк для c
-// func getDeletedStrings(ar1 []string, ar2 []string, arString [][]string) map[int] string {
-//     answer := map[int] string {}
-//     ind := -1
-//     for i := 0; i < len(ar1); i++ {
-//         count := 0
-//         for j := 0; j < len(ar2); j++ {
-//             if arString[i][j] == "ok" {
-//                 count++
-//             }
-//         }
-//         if count == 0 {
-//             answer[i+1] = "< " + ar1[i] + "\n"
-//             ind = i+1
-//         }
-//     }
-//     if ind != -1 {
-//         answer[ind] += "...\n"
-//     }
-//     return answer
-// }
-
-// // получение добавленных строк для c
-// func getAddedStrings(ar1 []string, ar2 []string, arString [][]string) map[int] string {
-//     answer := map[int] string {}
-//     for j := 0; j < len(ar2); j++ {
-//         flag := false
-//         for k := 0; k < len(ar1); k++ {
-//             if arString[k][j] != " " {
-//                 flag = true
-//             }
-//         }
-//         if !flag {
-//             answer[j+1] += "> " + ar2[j] + "\n"
-//         }
-//     }
-//     return answer
-// }
 
 // получение добавленных строк в начало для а
 func getBeginAddedString(ar1 []string, ar2 []string, arString [][]string) map[string] string {
@@ -156,28 +133,12 @@ func getEndAddedString(ar1 []string, ar2 []string, arString [][]string) map[stri
     return answer
 }
 
-func getC(ar1 []string, ar2 []string, arString [][]string) map[string] string {//getC(addedStrings map[int] string, deletedStrings map[int] string) map[int] string {
-    // answer := map[int] string {}
-    // key := 0
-    // for addedKey := range addedStrings {
-    //     for deletedKey := range deletedStrings {
-    //         if addedKey == deletedKey {
-    //             answer[addedKey] = deletedStrings[deletedKey]
-    //             key = addedKey
-    //         }
-    //     }
-    //     answer[key] += addedStrings[addedKey]
-    // }
-    // return answer
+func getC(ar1 []string, ar2 []string, arString [][]string) map[string] string {
     answer := map[string] string {}
     ind := 0
-    startInd := 1
+    startInd := 0
     str := ""
     keys1 := []string {}
-    // for key := range answer {
-    //     //ar := strings.Split(key, ",")
-    //     keys = append(keys, key) //ar[len(ar)-1])
-    // }
     for i := 0; i < len(ar1); i++ {
         count := 0
         for j := 0; j < len(ar2); j++ {
@@ -193,16 +154,16 @@ func getC(ar1 []string, ar2 []string, arString [][]string) map[string] string {/
                 str += "< " + ar1[i] + "\n"
             } else if i - ind > 1 {
                 keys1 = append(keys1, strconv.Itoa(startInd) + "," + strconv.Itoa(i))
-                answer[strconv.Itoa(i)] = str + "...\n"
+                answer[strconv.Itoa(startInd) + "," + strconv.Itoa(i)] = str + "...\n"
                 str = "< " + ar1[i] + "\n"
             }
             ind = i
         }
     }
-    if str != "" && startInd != 1 {
+    if str != "" && startInd != 0 {
         keys1 = append(keys1, strconv.Itoa(startInd) + "," + strconv.Itoa(ind+1))
-        answer[strconv.Itoa(ind+1)] = str + "...\n"
-    } else if str != "" && startInd == 1 {
+        answer[strconv.Itoa(startInd) + "," + strconv.Itoa(ind+1)] = str + "...\n"
+    } else if str != "" && startInd == 0 {
         keys1 = append(keys1, strconv.Itoa(startInd))
         answer[strconv.Itoa(startInd)] = str + "...\n"
     }
@@ -225,9 +186,11 @@ func getC(ar1 []string, ar2 []string, arString [][]string) map[string] string {/
                 }
                 str += "> " + ar2[i] + "\n"
             } else if i - ind > 1 {
-                //key = strconv.Itoa(startInd) + "," + strconv.Itoa(i)
-                keys2 = append(keys2, strconv.Itoa(startInd) + "," + strconv.Itoa(i))
-                localResult[strconv.Itoa(i)] = str + "...\n"
+                startInd = i+1
+                if str != "" {
+                    keys2 = append(keys2, strconv.Itoa(startInd) + "," + strconv.Itoa(i))
+                    localResult[strconv.Itoa(startInd) + "," + strconv.Itoa(i)] = str
+                }
                 str = "> " + ar2[i] + "\n"
             }
             ind = i
@@ -235,14 +198,32 @@ func getC(ar1 []string, ar2 []string, arString [][]string) map[string] string {/
     }
     if str != "" && startInd != 0 {
         keys2 = append(keys2, strconv.Itoa(startInd) + "," + strconv.Itoa(ind+1))
-        localResult[strconv.Itoa(ind+1)] = str + "...\n"
+        localResult[strconv.Itoa(startInd) + "," + strconv.Itoa(ind+1)] = str
     } else if str != "" && startInd == 0 {
         keys2 = append(keys2, strconv.Itoa(startInd))
-        localResult[strconv.Itoa(startInd)] = str + "...\n"
+        localResult[strconv.Itoa(startInd)] = str
     }
     result := map[string] string {}
-    for i := 0; i < len(keys1); i++ {
-        result[keys1[i] + "c" + keys2[i]] = answer[keys1[i]] + localResult[keys2[i]]
+    if len(keys1) == len(keys2) {
+        for i := 0; i < len(keys1); i++ {
+            splitKey := strings.Split(keys1[i], ",")
+            if splitKey[0] == splitKey[1] {
+                conv, _ := strconv.Atoi(splitKey[0])
+                result[strconv.Itoa(conv) + "c" + keys2[i]] = answer[keys1[i]] + localResult[keys2[i]]
+            } else {
+                result[keys1[i] + "c" + keys2[i]] = answer[keys1[i]] + localResult[keys2[i]]
+            }
+        }
+    } else {
+        for i := 0; i < len(keys1); i++ {
+            splitKey := strings.Split(keys1[i], ",")
+            conv, _ := strconv.Atoi(splitKey[0])
+            if splitKey[0] == splitKey[1] {
+                result[strconv.Itoa(conv) + "d" + strconv.Itoa(conv-1)] = answer[keys1[i]][:len(answer[keys1[i]])-4]
+            } else {
+                result[keys1[i] + "d" + strconv.Itoa(conv-1)] = answer[keys1[i]][:len(answer[keys1[i]])-4]
+            }
+        }
     }
     return result
 }
@@ -265,12 +246,8 @@ func main() {
     arString := getTable(ar1, ar2)
     fmt.Println(arString)
     beginAddedStrings := getBeginAddedString(ar1, ar2, arString)
-    // deletedStrings := getDeletedStrings(ar1, ar2, arString)
-    // addedStrings := getAddedStrings(ar1, ar2, arString)
-    // fmt.Println(deletedStrings)
-    // fmt.Println(addedStrings)
     endAddedStrings := getEndAddedString(ar1, ar2, arString)
-    changedStrings := getC(ar1, ar2, arString) //getC(addedStrings, deletedStrings)
+    changedStrings := getC(ar1, ar2, arString)
     fmt.Println(beginAddedStrings)
     fmt.Println(changedStrings)
     fmt.Println(endAddedStrings)
